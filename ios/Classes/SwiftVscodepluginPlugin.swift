b12f59277c76b6a9b4140bf51adaf95bbc63d199
susumOyaji <sumitomo0210@gmail.com>
2021-07-01 10:50:59 +0900

    up

--- ios/Classes/SwiftVscodepluginPlugin.swift
+++ ios/Classes/SwiftVscodepluginPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import PushKit
 
 public class SwiftVscodepluginPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -12,10 +13,27 @@ public class SwiftVscodepluginPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion": result("iOS " + UIDevice.current.systemVersion)
     case "getPlatformBattery": result("BATTERY " + String(UIDevice.current.batteryLevel))
-    case "getTelephonyInfo": result(UIDevice.current.model)
+    case "getTelephonyInfo": result(self.callNumber(phoneNumber:"7178881234"))
       break  
     default:result(FlutterMethodNotImplemented)
     }
     result("iOS " + UIDevice.current.systemVersion)
   }
+   ///case "getTelephonyInfo": result(UIDevice.current.model)
+   ///    result(callNumber(phoneNumber:"7178881234"))
+  private func callNumber(phoneNumber:String) {
+  if let url = URL(string: "tel://\(phoneNumber)"),
+   UIApplication.shared.canOpenURL(url) {
+      if #available(iOS 10, *) {
+        UIApplication.shared.open(url, options: [:], completionHandler:nil)
+       } else {
+           UIApplication.shared.openURL(url)
+       }
+   } else {
+            // add error message here 
+   }
+}
+  
+
+
 }
