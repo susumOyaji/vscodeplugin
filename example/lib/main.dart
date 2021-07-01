d3640f2da4647b8e066f539b6f649ebbd2cd8664
susumOyaji <sumitomo0210@gmail.com>
2021-07-01 10:54:13 +0900

    up

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,14 +42,14 @@ class _MyAppState extends State<MyApp> {
     } on PlatformException {
       platformBattery = 'Failed to get platform version.';
     }
-
+    /*
     try {
       platformTelephony =
           await Vscodeplugin.platformTelephony ?? 'Unknown platform version';
     } on PlatformException {
       platformTelephony = 'Failed to get platform version.';
     }
-
+    */
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
